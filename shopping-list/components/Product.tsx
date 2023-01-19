85d91adf4ccf7c05f4baec53d8407507912c7171
JyjyStudio <jyhad.benyoussef@gmail.com>
2023-01-19 13:36:25 +0100

    renamed file

---
+++ shopping-list/components/Product.tsx
@@ -0,0 +1,45 @@
+import { Text, StyleSheet, TouchableOpacity, Pressable } from 'react-native'
+import React from 'react'
+
+export default function Product({ product, deleteProduct }: Products) {
+	return (
+		<TouchableOpacity style={styles.productContainer}>
+			<Text style={styles.text}>{product.name}</Text>
+			<Pressable
+				onPress={() => {
+					deleteProduct(product.key)
+				}}
+			>
+				<Text style={styles.icon}>❌</Text>
+			</Pressable>
+		</TouchableOpacity>
+	)
+}
+
+const styles = StyleSheet.create({
+	productContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: 30,
+		paddingVertical: 50,
+		backgroundColor: 'lightgreen',
+		marginVertical: 5,
+	},
+	text: {
+		fontSize: 20,
+	},
+	icon: {
+		fontSize: 20,
+		marginRight: 10,
+	},
+})
+
+interface Product {
+	key: string
+	name: string
+}
+interface Products {
+	product: Product
+	deleteProduct: React.Dispatch<string>
+}
