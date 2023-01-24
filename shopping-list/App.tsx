a017a5b138d83d4c3522389d77101d1f34ff90f5
JyjyStudio <jyhad.benyoussef@gmail.com>
2023-01-24 12:56:46 +0100

    feat: use custom fonts

--- shopping-list/App.tsx
+++ shopping-list/App.tsx
@@ -4,7 +4,6 @@ import {
 	View,
 	FlatList,
 	Alert,
-	Text,
 	ImageBackground,
 } from 'react-native'
 import AddProductModal from './components/AddProductModal'
@@ -13,13 +12,9 @@ import Product from './components/Product'
 import ButtonComponent from './components/ButtonComponent'
 import Header from './components/Header'
 import colors from './constants/colors'
-import * as Font from 'expo-font'
+import { useFonts } from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
 
-const useFonts = async () =>
-	await Font.loadAsync({
-		pacifico: require('./assets/fonts/Pacifico-Regular.ttf'),
-	})
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync()
 
@@ -29,37 +24,25 @@ export default function App() {
 	>([])
 
 	const [modalVisible, setModalVisible] = useState(false)
-	const [appIsReady, setAppIsReady] = useState(false)
 
-	useEffect(() => {
-		async function prepare() {
-			try {
-				// Pre-load fonts, make any API calls you need to do here
-				await useFonts()
-			} catch (e) {
-				console.warn(e)
-			} finally {
-				// Tell the application to render
-				setAppIsReady(true)
-			}
-		}
-		prepare()
-	}, [])
+	const [fontsLoaded] = useFonts({
+		pacifico: require('./assets/fonts/Pacifico-Regular.ttf'),
+	})
 
 	const onLayoutRootView = useCallback(async () => {
-		if (appIsReady) {
-			// This tells the splash screen to hide immediately! If we call this after
-			// `setAppIsReady`, then we may see a blank screen while the app is
-			// loading its initial state and rendering its first pixels. So instead,
-			// we hide the splash screen once we know the root view has already
-			// performed layout.
+		if (fontsLoaded) {
+			// This tells the splash screen to hide immediately! If we call this after `setAppIsReady`,
+			// then we may see a blank screen while the app is loading its initial state and rendering
+			// its first pixels. So instead, we hide the splash screen once we know the root view
+			// has already performed layout.
 			await SplashScreen.hideAsync()
 		}
-	}, [appIsReady])
+	}, [fontsLoaded])
 
-	if (!appIsReady) {
+	if (!fontsLoaded) {
 		return null
 	}
+
 	const submitHandler = (product: string) => {
 		const idString = Date.now().toString()
 		if (product.length >= 2) {
@@ -92,10 +75,6 @@ export default function App() {
 			{ cancelable: true, onDismiss: () => console.warn('Annulé') }
 		)
 
-	const addCategory = (title: string) => (
-		<Text style={styles.categoryTitle}>{title}</Text>
-	)
-
 	return (
 		<DismissKeyboard>
 			<ImageBackground
@@ -117,18 +96,6 @@ export default function App() {
 								'rgb(130, 230, 244)',
 								'rgb(140, 180, 254)',
 								'rgb(190, 120, 264)',
-							]}
-							start={{ x: 0.5, y: 0 }}
-							end={{ x: 0.5, y: 1 }}
-						/>
-						<ButtonComponent
-							title="Ajouter une catégorie"
-							onPress={() => addCategory('test')}
-							style={styles.grandientBtn}
-							colors={[
-								'rgb(162, 224, 146)',
-								'rgb(102, 244, 186)',
-								'rgb(51, 139, 147)',
 							]}
 							start={{ x: 0.5, y: 0 }}
 							end={{ x: 0.5, y: 1 }}
@@ -174,7 +141,7 @@ const styles = StyleSheet.create({
 		marginTop: 30,
 		justifyContent: 'space-between',
 		alignItems: 'center',
-		minHeight: 20,
+		// minHeight: 20,
 	},
 	grandientBtn: {
 		width: '100%',
@@ -187,12 +154,8 @@ const styles = StyleSheet.create({
 		fontSize: 17,
 		color: colors.light,
 	},
-	categoryTitle: {
-		fontWeight: 'bold',
-		fontSize: 20,
-	},
 	flatlist: {
-		height: '50%',
+		height: '65%',
 		marginTop: 10,
 	},
 })
