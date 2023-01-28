20ad2220ee6806b8ca9516b428789614a692387a
JyjyStudio <jyhad.benyoussef@gmail.com>
2023-01-28 15:34:51 +0100

    defaultNavigationOptions

--- pixels/routes/HomeStackNav.ts
+++ pixels/routes/HomeStackNav.ts
@@ -2,19 +2,34 @@ import { createAppContainer } from "react-navigation"
 import { createStackNavigator } from "react-navigation-stack"
 import Home from "../screens/Home"
 import Photos from "../screens/Photos"
-import Portfolio from "../screens/Portfolio"
+import Profile from "../screens/Profile"
 
 const screens = {
 	Home: {
 		screen: Home,
 	},
-	Portfolio: {
-		screen: Portfolio,
+	Profile: {
+		screen: Profile,
 	},
 	Photos: {
 		screen: Photos,
 	},
 }
-const StackNav = createStackNavigator(screens)
+
+const defaultNavigationOptions = {
+	defaultNavigationOptions: {
+		headerStyle: {
+			backgroundColor: "salmon",
+		},
+		headerTintColor: "#fff",
+		headerTitleStyle: {
+			fontSize: 30,
+			// textAlign: "center",
+			fontFamily: "Ubuntu_700Bold",
+		},
+	},
+}
+
+const StackNav = createStackNavigator(screens, defaultNavigationOptions)
 
 export default createAppContainer(StackNav)
