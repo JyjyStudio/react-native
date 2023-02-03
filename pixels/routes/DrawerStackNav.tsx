f7aad41f05cafd0fdd0943d59d537a30172ce932
unknown <jyhad.benyoussef@gmail.com>
2023-02-03 14:39:57 +0100

    feat: drawerStackNav (not working with last expo version)

--- pixels/routes/DrawerStackNav.tsx
+++ pixels/routes/DrawerStackNav.tsx
@@ -1,22 +1,38 @@
 import React from "react"
-import { createDrawerNavigator } from "@react-navigation/drawer"
-import { NavigationContainer } from "@react-navigation/native"
+import { createDrawerNavigator } from "react-navigation-drawer"
+import { createAppContainer } from "react-navigation"
 import Faq from "../screens/Faq"
-import Home from "../screens/Home"
+import BottomTabNav from "./BottomTabNav"
+import colors from "../constants/colors"
 
-
-const Drawer = createDrawerNavigator()
-
-export default function DrawerStackNav() {
-	return (
-		<NavigationContainer>
-			<Drawer.Navigator initialRouteName="Home">
-				<Drawer.Screen name="Home" component={Home} />
-				<Drawer.Screen
-					name="FAQ"
-					component={Faq}
-				/>
-			</Drawer.Navigator>
-		</NavigationContainer>
-	)
+const RouteConfig = {
+	Home: {
+		screen: BottomTabNav,
+		navigationOptions: {
+			drawerLabel: "Accueil",
+		},
+	},
+	Faq: {
+		screen: Faq,
+		navigationOptions: {
+			drawerLabel: "FAQ",
+		},
+	},
 }
+
+const DrawerNavigatorConfig = {
+	hideStatusBar: true,
+	drawerBackgroundColor: colors.dark,
+	drawerWidth: 110,
+	drawerType: "slide",
+	contentOptions: {
+		labelStyle: {
+			fontSize: 19,
+		},
+		inactiveTintColor: colors.light,
+		activeTintColor: colors.selected,
+	},
+}
+
+const MainNavigator = createDrawerNavigator(RouteConfig, DrawerNavigatorConfig)
+export default createAppContainer(MainNavigator)
