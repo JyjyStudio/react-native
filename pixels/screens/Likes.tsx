e9ff225dbfb29683240b5523955bb09148ce86bf
unknown <jyhad.benyoussef@gmail.com>
2023-02-04 14:39:16 +0100

    feat: new page

---
+++ pixels/screens/Likes.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+import React from "react"
+import globalStyle from "../constants/globalStyle"
+
+export default function Likes() {
+	return (
+		<View style={globalStyle.container}>
+			<Text style={globalStyle.bodyTitle}>Account</Text>
+		</View>
+	)
+}
