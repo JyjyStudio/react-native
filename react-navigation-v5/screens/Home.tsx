ec87879388d5873afe2bfb113b5e3a5ca05b72cf
unknown <jyhad.benyoussef@gmail.com>
2023-02-23 20:21:50 +0100

    feat: Homepage with navigation

--- react-navigation-v5/screens/Home.tsx
+++ react-navigation-v5/screens/Home.tsx
@@ -1,16 +1,46 @@
-import { StyleSheet, Text, View } from "react-native"
 import React from "react"
-import { Link } from "@react-navigation/native"
+import { Pressable, StyleSheet, Text, View } from "react-native"
+import { NavigationProps } from "../constants/types"
 
-export default function Home() {
+export default function ({ navigation }: NavigationProps) {
+	const handlePress = () => {
+		navigation.navigate("Portfolio")
+	}
+
 	return (
-		<View
-			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-		>
-			<Text>Home Screen</Text>
-			<Link to="/Portfolio">Portfolio Screen</Link>
+		<View style={styles.container}>
+			<Text style={styles.text}>Home</Text>
+			<Pressable
+				style={({ pressed }) => ({
+					backgroundColor: pressed ? "lightseagreen" : "rebeccapurple",
+					...styles.pressable,
+				})}
+				onPress={handlePress}
+			>
+				<Text style={styles.btn}>Vers Portfolio</Text>
+			</Pressable>
 		</View>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "lightblue",
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	pressable: {
+		marginVertical: 15,
+		borderRadius: 15,
+	},
+	text: {
+		fontSize: 24,
+	},
+	btn: {
+		paddingVertical: 7,
+		paddingHorizontal: 16,
+		color: "white",
+		fontSize: 18,
+	},
+})
