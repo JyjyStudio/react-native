e78c5e549ab43acb749eae2751218c802cfcbebe
unknown <jyhad.benyoussef@gmail.com>
2023-02-25 17:30:23 +0100

    clean code + data from route

--- react-navigation-v5/screens/Portfolio.tsx
+++ react-navigation-v5/screens/Portfolio.tsx
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from "react-native"
+import { Text, View, TouchableOpacity } from "react-native"
 import React from "react"
+import { NavigationProps } from "../constants/types"
+import { Globalstyle } from "../constants/globalStyle"
 
-export default function Portfolio() {
+export default function Portfolio({ navigation, route }: NavigationProps) {
+	const user = route.params
 	return (
-		<View
-			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-		>
-			<Text>Portfolio Screen</Text>
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text style={Globalstyle.bodyTxt}>Profil de {user.userId}</Text>
+			<TouchableOpacity
+				style={{ ...Globalstyle.btn, backgroundColor: "lightgreen" }}
+				onPress={() => navigation.navigate("Home")}
+				activeOpacity={0.7}
+			>
+				<Text style={{ ...Globalstyle.buttonTxt, color: "black" }}>
+					Retourner vers Home
+				</Text>
+			</TouchableOpacity>
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({})
